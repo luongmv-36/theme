82991bee4cc4c8c4a2f3eacd527982c9d461a511
Luong Mai Van <maivanluong1@gmail.com>
2018-01-02 13:20:02 +0700

    setuptheme

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css"  href="css/dev.css"/>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+            <ul class="nav nav-tabs">
+                    <li class="active col-xs-4"><a data-toggle="tab" href="#home">Home</a></li>
+                    <li class="col-xs-4"><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+                    <li class="col-xs-4"><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+            </ul>
+        </div>
+
+    <div class="tab-content">
+        <div id="home" class="tab-pane fade in active">
+            <h3>HOME</h3>
+            <div class="container">
+                <div class="media">
+                    <div class="media-left media-top">
+                        <img src="img/img_avatar1.png" class="media-object" style="width:80px">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">Media Top</h4>
+                        <h5>
+                            <code>PHP</code>
+                            <code>Java</code>
+                        </h5>
+                        <p>
+                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
+                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
+                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
+                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
+                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
+                        </p>
+                        <span>
+                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
+                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
+                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
+                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
+                        </span>
+                    </div>
+                </div>
+                <hr>
+                <div class="media">
+                    <div class="media-left media-top">
+                        <img src="img_avatar1.png" class="media-object" style="width:80px">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">Media Top</h4>
+                        <h5>
+                            <code>PHP</code>
+                            <code>Java</code>
+                            <code>NodeJs</code>
+                        </h5>
+                        <p>
+                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
+                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
+                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
+                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
+                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
+                        </p>
+                        <span>
+                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
+                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
+                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
+                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
+                        </span>
+                    </div>
+                </div>
+                <hr>
+                <div class="media">
+                    <div class="media-left media-top">
+                        <img src="img_avatar1.png" class="media-object" style="width:80px">
+                    </div>
+                    <div class="media-body">
+                        <h4 class="media-heading">Media Top</h4>
+                        <h5>
+                            <code>PHP</code>
+                            <code>Java</code>
+                        </h5>
+                        <p>
+                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
+                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
+                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
+                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
+                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
+                        </p>
+                        <span>
+                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
+                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
+                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
+                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
+                        </span>
+                    </div>
+                </div>
+                <hr>
+
+            </div>
+        </div>
+        <div id="menu1" class="tab-pane fade">
+            <h3>Menu 1</h3>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div id="menu2" class="tab-pane fade">
+            <h3>Menu 2</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+<style>
+    body {
+        font-family: Segoe UI, Frutiger, Frutiger Linotype, Dejavu Sans, Helvetica Neue, Arial, sans-serif;
+        font-size: 14px;
+        font-style: normal;
+        font-variant: normal;
+        font-weight: 400;
+        line-height: 20px;
+    }
+</style>
