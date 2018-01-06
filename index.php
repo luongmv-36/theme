22b81e60fb9ba14aa32fdfb38a3dbd1556371514
Luong Mai Van <maivanluong1@gmail.com>
2018-01-06 15:55:14 +0700

    style update

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css"  href="css/dev.css"/>
+    <link rel="stylesheet" href="plugin/font-awesome-animation.min.css">
 </head>
 <body>
 
@@ -38,85 +39,86 @@
         <div id="home" class="tab-pane fade in active">
             <h3>HOME</h3>
             <div class="container">
-                <div class="media">
+                <!--start-->
+                <div class="media item_works" style="">
                     <div class="media-left media-top">
-                        <img src="img/img_avatar1.png" class="media-object" style="width:80px">
+                        <img class="" src="img/magento.png" class="media-object" style="width:50px">
                     </div>
                     <div class="media-body">
-                        <h4 class="media-heading">Media Top</h4>
+                        <h4 class="media-heading">
+                            <a href="#" style="color: black">Media Top</a>
+                            <i class="fa fa-circle-o faa-burst animated"></i>
+                        </h4>
                         <h5>
+                            <i class="fa fa-check-circle-o" style="color: #1C9A71;border-right:1px solid black; padding-right: 4px" aria-hidden="true"><small style="padding-left: 2px">Đang Tuyển</small></i>
                             <code>PHP</code>
                             <code>Java</code>
+                            <i class="fa fa-star-o faa-falling animated" style="border-left:1px solid black; padding-left: 4px";color:sandybrown"><small>Mới</small></i>
                         </h5>
                         <p>
-                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
-                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
-                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
-                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
-                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
+                            <div class="row">
+                                <div class="col-sm-9">
+                                From shades of color to specific formularies, custom product attributes are essential when matching products to customer needs.
+                                Magento helps you quickly and easily showcase your unique product attributes and tap robust product bundling to grow your average order value.
+                                From shades of color to specific formularies, custom product attributes are essential when matching products to customer needs.
+                                Magento helps you quickly and easily showcase your unique product attributes and tap robust product bundling to grow your average order value.
+                                </div>
+                            <div class="col-sm-3">
+                                <img class="" src="img/magento2.png" class="img-responsive" style="height: 130px;float: right">
+                            </div>
+                            </div>
                         </p>
                         <span>
-                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
-                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
-                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
-                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
+                            <i class="fa fa-comment fa_icon" style="color: red" aria-hidden="true">129 Comment</i>
+                            <i class="fa fa-thumbs-up fa_icon" style="color: blue" aria-hidden="true">200 Like</i>
+                            <i class="fa fa-eye fa_icon" aria-hidden="true" style="color: darkgray">490 View</i>
+                            <i class="fa fa-clock-o fa_icon" style="color: grey" aria-hidden="true">20-10-2018</i>
+                            <a href="#" class="fa_read"><i class="fa fa-play-circle-o fa_icon" aria-hidden="true" title="Click vào để đọc tiếp tin này!">Đọc Tin...</i></a>
+                            <i class="fa fa-money fa_icon" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
                         </span>
                     </div>
                 </div>
-                <hr>
-                <div class="media">
+                <!--and-->
+                <!--start-->
+                <div class="media item_works">
                     <div class="media-left media-top">
-                        <img src="img_avatar1.png" class="media-object" style="width:80px">
+                        <img class="" src="img/magento.png" class="media-object" style="width:50px">
                     </div>
                     <div class="media-body">
-                        <h4 class="media-heading">Media Top</h4>
+                        <h4 class="media-heading">
+                            <a href="#" style="color: black">Media Top</a>
+                            <i class="fa fa-circle-o faa-burst animated"></i>
+                        </h4>
                         <h5>
+                            <i class="fa fa fa-ban" style="color: #F0672F;border-right:1px solid black; padding-right: 4px" aria-hidden="true"><small style="padding-left: 2px">Đã Tuyển</small></i>
                             <code>PHP</code>
                             <code>Java</code>
-                            <code>NodeJs</code>
+                            <i class="fa fa-star-o faa-falling animated" style="border-left:1px solid black; padding-left: 4px";color:sandybrown"><small>Mới</small></i>
                         </h5>
                         <p>
-                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
-                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
-                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
-                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
-                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
+                        <div class="row">
+                            <div class="col-sm-9">
+                                From shades of color to specific formularies, custom product attributes are essential when matching products to customer needs.
+                                Magento helps you quickly and easily showcase your unique product attributes and tap robust product bundling to grow your average order value.
+                                From shades of color to specific formularies, custom product attributes are essential when matching products to customer needs.
+                                Magento helps you quickly and easily showcase your unique product attributes and tap robust product bundling to grow your average order value.
+                            </div>
+                            <div class="col-sm-3">
+                                <img class="" src="img/magento2.png" class="img-responsive" style="height: 130px;float: right">
+                            </div>
+                        </div>
                         </p>
                         <span>
-                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
-                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
-                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
-                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
+                            <i class="fa fa-comment fa_icon" style="color: red" aria-hidden="true">129 Comment</i>
+                            <i class="fa fa-thumbs-up fa_icon" style="color: blue" aria-hidden="true">200 Like</i>
+                            <i class="fa fa-eye fa_icon" aria-hidden="true" style="color: darkgray">490 View</i>
+                            <i class="fa fa-clock-o fa_icon" style="color: grey" aria-hidden="true">20-10-2018</i>
+                            <a href="#" class="fa_read"><i class="fa fa-play-circle-o fa_icon" aria-hidden="true" title="Click vào để đọc tiếp tin này!">Đọc Tin...</i></a>
+                            <i class="fa fa-money fa_icon" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
                         </span>
                     </div>
                 </div>
-                <hr>
-                <div class="media">
-                    <div class="media-left media-top">
-                        <img src="img_avatar1.png" class="media-object" style="width:80px">
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">Media Top</h4>
-                        <h5>
-                            <code>PHP</code>
-                            <code>Java</code>
-                        </h5>
-                        <p>
-                            Em đi làm 1 năm ngành này mà trước h em làm cho công ty sản xuất. Cty nó có nhiều team IT và nó xài nhiều công nghệ,
-                            mà toàn cũ công nghệ từ 2k8 2k9. Team em thì được mới hơn tý là làm Angular 1.0.2 với NodeJS. Lâu lâu lại chen vào winform,
-                            nên em chả chuyên vào cái ngôn ngữ nào hết. Nhiều khi đang là Angular nó lại đưa cái task winfrom đời tống ngồi làm.
-                            Chả ai tranning gì cả. Làm thì tự mò vì team bên kia người Đài Loan họ lại ít nói tiếng anh. Hỏi thì nhiều khi k thèm trả lời.
-                            Nên đi làm 1 năm nay thấy cũng chả tiếp thu được gì nhiều. Lên trình đọc hiểu code với copy paste thôi
-                        </p>
-                        <span>
-                            <i class="fa fa-comment" style="color: red" aria-hidden="true">129 Comment </i>
-                            <i class="fa fa-thumbs-up" style="color: blue" aria-hidden="true">200 Like </i>
-                            <i class="fa fa-eye" aria-hidden="true" style="color: darkgray">490 View </i>
-                            <i class="fa fa-money" style="float: right;color: olivedrab" aria-hidden="true"> <span class="label label-info">700$-1000$</span></i>
-                        </span>
-                    </div>
-                </div>
-                <hr>
+                <!--and-->
 
             </div>
         </div>
@@ -141,5 +143,20 @@
         font-variant: normal;
         font-weight: 400;
         line-height: 20px;
+    }
+    .fa_icon{
+        margin-right: 3px;
+    }
+    a .fa_icon:hover{
+        text-decoration: underline;
+    }
+    body{
+        background-color: #FAFAFA;
+    }
+    .item_works{
+        border: 1px solid;
+        background: #FFFFFF;
+        border-radius: 5px;
+        border: 1px solid #dddfe2;
     }
 </style>
